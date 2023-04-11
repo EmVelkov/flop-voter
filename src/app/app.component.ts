0bb1f3a5d2ab779ffd99eca8c197932f97babb55
Emiliyan Velkov <emiliyanweb@gmail.com>
2023-04-11 09:42:20 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'flopbuster';
+  selectedIndex: number = -1;
+
+  isSelected(index: any) {
+    return this.selectedIndex === index;
+  }
+
+  anySelected() {
+    return this.selectedIndex !== -1;
+  }
+
+  onVoteClick(index: any) {
+    if (this.isSelected(index)) {
+      this.selectedIndex = -1;
+    } else {
+      this.selectedIndex = index;
+    }
+  }
+  getButtonText(index: any) {
+    if (this.isSelected(index)) {
+      return 'Unvote';
+    } else {
+      return 'Vote';
+    }
+  }
 }
